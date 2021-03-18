d9e281c498c5f6e16bfb4e499487eae9b572b650
Alex Levente Socosan <alex.socosan@stud.uni-corvinus.hu>
2021-03-18 15:36:17 +0100

    jsgyak

--- HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -23,6 +23,7 @@ window.onload = () => {
             else {
                 oszlop.innerText = faktoriális(i) / (faktoriális(j) * faktoriális(i - j));
                 oszlop.style.background = `rgb(150,100,${120 + faktoriális(i) / (faktoriális(j) * faktoriális(i - j))}`;
+
             }
         }
     }
